e2f3f91e351c4bc5a86b77d3255cb27a04f63cad
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-02-28 12:54:45 +0100

    Update runfile_working_first.C

--- runfile_working_first.C
+++ runfile_working_first.C
@@ -1,58 +1,92 @@
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <math.h>
-#include "Riostream.h"
-#include <vector>
-#include <TString.h>
-#include "hiss.C"
 using namespace std;
 
-void runfile_working_first(TString path, TString filename)
-	{ 
-	
-    TCanvas *c1 = new TCanvas( "c1", "" );
-	c1->SetLogy();
-	
-	vector< vector<TString> > PED_LED;
-	vector<TString> a;
-	a.clear();
-	a.push_back("/PED/");
-	a.push_back("/LED/");
-	PED_LED.push_back(a);
-	a.clear();
-	
-	vector< vector<TString> > HV;
-	vector<TString> b;
-	b.clear();
-	b.push_back("1250");
-	b.push_back("1300");
-	b.push_back("1350");
-	b.push_back("1401");
-	b.push_back("1450");
-	HV.push_back(b);
-	
-	int i=0;
-	int j=0;
-	int PED_LED_Count =2;
-	int HV_Count = 5;
-	int num =1;
-	while (i<HV_Count)
+//main function
+TH1F* hiss(TString path, TString filename){
+  
+  TString histname = filename;
+  histname.ReplaceAll(".",""); //remove all "."s from histname
+  histname.ReplaceAll("/",""); //remove all "/"s from histname
+  ifstream read;
+    read.open((path+filename).Data());
+    //read.open("test.txt");
+    double charge;
+    double amplitude;
+    double charge_min;
+    double charge_max;
+    double bin_width;
+    int count=0;
+//ignore first 5 lines
+    for (int a=0; a<5; a++){
+        read.ignore(1000000000000, '\n');
+        }	
+//read to find charge_max and charge_min
+    while(read >> charge >> amplitude)
 	{
-		while (j<PED_LED_Count)
+     	//cout << "charge = " << charge << " " << "amplitude= " << amplitude << endl;
+		
+        if (count == 0)
 		{
-			hiss(path + HV[0][i] + PED_LED[0][j] , filename);
-			
-			c1->Update();
-			c1->WaitPrimitive();
-			
-			++j;
-			++num;
-		}
-		++i;
-		j=0;
-	}
+            charge_min = charge;
+			//cout << "charge min = " << charge_min << endl;
+        }
+        if (count == 1)
+		{
+            bin_width = charge - charge_min;
+		//	cout << "bin_width = " << bin_width << endl;
+        }
 
+        charge_max = charge;
+        ++count;
+    }
 	
-	}
+    //cout << charge_min << " " << charge_max <<" "<<bin_width<< endl;
+
+//start calculations
+	double charge_min_temp;
+	charge_min_temp = charge_min;
+    charge_min = -1*charge_max*1.0e+9;
+    charge_max = -1*charge_min_temp*1.0e+9;
+    bin_width  = bin_width*1.0e+9;
+ 
+	//cout << charge_min << " " << charge_max <<" "<<bin_width<< "  " << count << endl;
+//create and allocate histogram
+
+   read.close();
+   
+   
+//start histogram event		
+  TH1F* hist = new TH1F(path+histname, histname, count , charge_min-(bin_width/2), charge_max+(bin_width/2));
+	
+	ifstream scan;
+    scan.open((path+filename).Data());
+//ignore first 5 lines	
+	for (int a=0; a<5; a++){
+        scan.ignore(1000000000000, '\n');
+        }
+//fill	histogram while calculating each element	
+    while(scan >> charge >> amplitude){
+      
+      charge = -1*charge*1.0e+9;
+	 // cout << "charge = " << charge << " " << "amplitude= " << amplitude << endl;
+      hist -> Fill(charge, amplitude/2);
+  }
+//std::string name ="result_" + std::string (filename) + ".root";
+//scan2.close();
+  TFile* file = new TFile(path + "root_file.root","RECREATE"); //new ROOT file event
+  hist->GetXaxis()->SetTitle("charge(nC)"); //set Xaxis title
+  hist->GetYaxis()->SetTitle("amplitude"); //set Yaxis title
+  hist->Draw();
+  file->Write(); //write to ROOT file
+  file->Close();
+//modify and update the canvas  
+  gPad->Modified(); 
+  gPad->Update(); 
+  gSystem->ProcessEvents();
+  return hist;
+  return (0);
+  
+}
