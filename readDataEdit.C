67835864c171826353319b56edebe52ae32da771
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-04-01 15:20:49 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -23,54 +23,24 @@ int main(int argc, char ** argv)
 	TFile * f = TFile::Open(argv[1]);
 	TTree * pmt_tree = (TTree*) f->Get("pmt_tree");
 	TVectorT<float> * time_vector = (TVectorT<float> *) f->Get("time_vector");
-
-	// for (int k =0; k<10; ++k)
-	// {
-	//cout<< time_vector[0]<<endl;
-	// }
 	std::vector<float> * wave_vector = 0;
-
 	pmt_tree->SetBranchAddress("wave_vector", &wave_vector);
-	//pmt_tree->SetBranchAddress("time_vector", &time_vector);
-	// pmt_tree-> Print();
-	// pmt_tree-> Scan("wave_vector");
 	TApplication TApp("TApp", &argc, argv);
 
 	TCanvas * c = new TCanvas();
 
 	int count = 2000;
-	float t_min = 0;
-	float t_max = 0;
+	float t_min = 400;
+	float t_max = 550;
 	float Integral = 0;
 	float bin_width = 0;
 
 //determining the bin width, the times (x value) corresponding to 400ns and 550ns, 
 //and the no. of bins (count) within the range 
 
-	// for (int j=0; j < wave_vector->size(); ++j)
-	// 		{
-	// 			if (( *time_vector >= 400 ) && ( *time_vector < 550))
-	// 				{
-	// 					if (count ==0)
-	// 					{
-	// 						cout<<"hall!"<<endl;
-	// 						t_min =  *time_vector;
-	// 					}
-	// 					if (count ==1)
-	// 					{
-	// 						bin_width =  *time_vector- t_min;
-	// 						cout<<"hall! hall!"<<endl;
-	// 					}
-
-	// 					++count;
-	// 					t_max = *time_vector;
-	// 				}
-				
-	// 		}
-
-
 //definition of the histogram using previous findings
-	TH1F *Juno = new TH1F("Juno", "Juno", count , 400, 550);
+TH1F *Juno = new TH1F("Juno", "Juno", count , t_min, t_max);
+	
 
 	for(int i=0; i < pmt_tree->GetEntries(); ++i)
 		{
@@ -80,8 +50,12 @@ int main(int argc, char ** argv)
 		TVectorT<float> wave_vector_root(wave_vector->size());
 		for(int iWV=0; iWV < wave_vector->size(); ++iWV)
 			{
-				wave_vector_root[iWV] = wave_vector->at(iWV);
-				Juno-> Fill(wave_vector_root[iWV]);
+				wave_vector_root[iWV] = wave_vector->at(iWV); 
+				if ( ( (*time_vector)(j) >= t_min ) && ( (*time_vector)(j) < t_max) )
+					{
+						Integral += (*time_vector)(j); 
+					}
+				Juno-> Fill(Integral);
 				
 			}
 			
@@ -94,7 +68,6 @@ int main(int argc, char ** argv)
 		g->SetMarkerStyle(24);
 		g->SetMarkerColor(kBlue);
 		g->Draw("ALP");
-		
 		Juno->Draw();
 		c->Update();
 		c->WaitPrimitive();
