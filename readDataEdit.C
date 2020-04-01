a045dd303ed22e8e1fc0c2eb77f598feb64493d0
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-04-01 16:09:42 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -29,7 +29,7 @@ int main(int argc, char ** argv)
 
 	TCanvas * c = new TCanvas();
 
-	int nbins = 10000;
+	int nbins = 1000;
 	float t_min = 400;
 	float t_max = 550;
 	float Integral = 0;
@@ -39,7 +39,7 @@ int main(int argc, char ** argv)
 //and the no. of bins (count) within the range 
 
 //definition of the histogram using previous findings
-TH1F *Juno = new TH1F("Juno", "Juno", nbins , -5000, 5000);
+TH1F *Juno = new TH1F("Juno", "Juno", nbins , -6000, 2000);
 
 	for(int i=0; i < pmt_tree->GetEntries(); ++i)
 		{
