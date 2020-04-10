1dbe935dd3b321d55d42ff7734d02653077f824d
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-04-10 13:55:05 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -20,102 +20,138 @@ int main(int argc, char ** argv)
 		std::cout << "Usage: " << argv[0] << " file.root" << std::endl;
 		return 1;
 	}
+	
+	TString PED;
+	TString LED;
+	TString Nom;
+				
+	for (int p = 0; p<8; ++p)
+	{
+				
+		//run through each folder and file	
+		Nom = TString("/scan3737/scan3737_position") + Form("%d", p) + TString("_angle");
+		
+		for (int a = 0; a<24; ++a)
+			{				
 
-	TFile * f = TFile::Open(argv[1]);
+				TString filename;
+				TString histname;
+				
+				TString histname_LED;
+				TString histname_PED;
 
-	TTree * pmt_tree = (TTree*) f->Get("pmt_tree");
-	TVectorT<float> * time_vector = (TVectorT<float> *) f->Get("time_vector");
-	std::vector<float> * wave_vector = 0;
-	pmt_tree->SetBranchAddress("wave_vector", &wave_vector);
-	TApplication TApp("TApp", &argc, argv);
+				TString filename_LED;
+				TString filename_PED;
 
-	TCanvas * c = new TCanvas();
+				filename = Nom + Form("%d", 15*a) + TString(".root");
+				histname = TString("position = ") + Form("%d",p) + TString("angle = ")+ Form("%d", 15*a);
+				
+				histname_LED = histname + TString(" (LED)");
+				histname_PED = histname + TString(" (PED)");
+				
+				filename_LED = Nom + Form("%d", 15*a) + TString("_LED.txt");
+				filename_PED = Nom + Form("%d", 15*a) + TString("_PED.txt");
 
-	int nbins = 2000;
-	float t_min = 400;
-	float t_max = 500;
-	float t_min_PED = 0;
-	float t_max_PED = 100;
-	float Integral = 0;
-	float Integral_PED = 0 ;
-	float bin_width = 0;
 
-//definition of the histogram
-TH1F *Juno = new TH1F("Juno", "Juno", nbins , -8000, 1000);
-TH1F *JunoPED = new TH1F("JunoPED", "JunoPED", nbins , -8000, 1000);
+				TFile * f = TFile::Open(filename);
+				TTree * pmt_tree = (TTree*) f->Get("pmt_tree");
+				TVectorT<float> * time_vector = (TVectorT<float> *) f->Get("time_vector");
+				std::vector<float> * wave_vector = 0;
+				pmt_tree->SetBranchAddress("wave_vector", &wave_vector);
+				TApplication TApp("TApp", &argc, argv);
 
-	for(int i=0; i < pmt_tree->GetEntries(); ++i)
-		{
+				TCanvas * c = new TCanvas();
 
-		pmt_tree->GetEntry(i);
+				int nbins = 2000;
+				float t_min = 400;
+				float t_max = 500;
+				float t_min_PED = 0;
+				float t_max_PED = 100;
+				float Integral = 0;
+				float Integral_PED = 0 ;
+				float bin_width = 0;
 
-		TVectorT<float> wave_vector_root(wave_vector->size());
-		for(int iWV=0; iWV < wave_vector->size(); ++iWV)
-			{
-				wave_vector_root[iWV] = wave_vector->at(iWV); 
-				if ( ( (*time_vector)(iWV) >= t_min ) && ( (*time_vector)(iWV) < t_max) )
+				//definition of the histogram
+				TH1F *Juno = new TH1F("Juno", "histname_LED", nbins , -8000, 1000);
+				TH1F *JunoPED = new TH1F("JunoPED", "histname_PED", nbins , -8000, 1000);
+
+				for(int i=0; i < pmt_tree->GetEntries(); ++i)
 					{
-						Integral += wave_vector_root[iWV]; 
-					}
-				if ( ( (*time_vector)(iWV) >= t_min_PED ) && ( (*time_vector)(iWV) < t_max_PED) )
+
+					pmt_tree->GetEntry(i);
+
+					TVectorT<float> wave_vector_root(wave_vector->size());
+					for(int iWV=0; iWV < wave_vector->size(); ++iWV)
+						{
+							wave_vector_root[iWV] = wave_vector->at(iWV); 
+							if ( ( (*time_vector)(iWV) >= t_min ) && ( (*time_vector)(iWV) < t_max) )
+								{
+									Integral += wave_vector_root[iWV]; 
+								}
+							if ( ( (*time_vector)(iWV) >= t_min_PED ) && ( (*time_vector)(iWV) < t_max_PED) )
+								{
+									Integral_PED += wave_vector_root[iWV]; 
+								}	
+						}
+					Juno-> Fill(Integral);
+					JunoPED-> Fill(Integral_PED);	
+					//cout<<"Integral at Entry no: "<< i << " = "<< Integral <<endl;
+					//TGraph * g = new TGraph(*time_vector, wave_vector_root);
+					//g->GetXaxis()->SetTitle("Time (ns)");
+					//g->GetYaxis()->SetTitle("Amplitude");
+					Juno->GetXaxis()->SetTitle("Integral");
+					Juno->GetYaxis()->SetTitle("Counts");
+					JunoPED->GetXaxis()->SetTitle("Integral");
+					JunoPED->GetYaxis()->SetTitle("Counts");
+					//g->SetTitle(TString::Format("Event %d", i));
+					//g->SetMarkerSize(.5);
+					//g->SetMarkerStyle(24);
+					//g->SetMarkerColor(kBlue);
+					//g->Draw("ALP");
+
+					//delete g;
+					Integral = 0;
+					Integral_PED = 0;
+				}
+
+					JunoPED->Draw();
+					c->Update();
+					c->WaitPrimitive();
+
+					Juno->Draw();
+
+				 ofstream ff (filename_LED);
+				 ff <<"Juno PMT data"<<endl;
+				 ff <<"Histogram of Integral vs. Counts"<<endl;
+				 ff <<"No. of bins = "<<nbins<<endl;
+				 ff <<"position = "<< p <<" , angle = "<<15*a<<endl;
+				 ff <<"Integral"<<" "<<"counts"<<endl;
+
+				 ofstream ffP (filename_PED);
+				 ffP <<"Juno PMT data - Pedestal"<<endl;
+				 ffP <<"Histogram of Integral vs. Counts - Pedestal"<<endl;
+				 ffP <<"No. of bins = "<<nbins<<endl;
+				 ff <<"position = "<< p <<" , angle = "<<15*a<<endl;
+				 ffP <<"Integral"<<" "<<"counts"<<endl;
+				 
+				 for (int i=0; i <Juno->GetNbinsX(); i++)
 					{
-						Integral_PED += wave_vector_root[iWV]; 
-					}	
-			}
-		Juno-> Fill(Integral);
-		JunoPED-> Fill(Integral_PED);	
-		//cout<<"Integral at Entry no: "<< i << " = "<< Integral <<endl;
-		//TGraph * g = new TGraph(*time_vector, wave_vector_root);
-		//g->GetXaxis()->SetTitle("Time (ns)");
-		//g->GetYaxis()->SetTitle("Amplitude");
-		Juno->GetXaxis()->SetTitle("Integral");
-		Juno->GetYaxis()->SetTitle("Counts");
-		JunoPED->GetXaxis()->SetTitle("Integral");
-		JunoPED->GetYaxis()->SetTitle("Counts");
-		//g->SetTitle(TString::Format("Event %d", i));
-		//g->SetMarkerSize(.5);
-		//g->SetMarkerStyle(24);
-		//g->SetMarkerColor(kBlue);
-		//g->Draw("ALP");
+				        ff << Juno->GetBinCenter(i) << "	" << Juno->GetBinContent(i) << endl; //write to file
+				  	}
+				  	ff.close();
 
-		//delete g;
-		Integral = 0;
-		Integral_PED = 0;
-	}
-
-		JunoPED->Draw();
-		c->Update();
-		c->WaitPrimitive();
-
-		Juno->Draw();
-
-	 ofstream ff ("Juno_data.txt");
-	 ff <<"Juno PMT data"<<endl;
-	 ff <<"Histogram of Integral vs. Counts"<<endl;
-	 ff <<"No. of bins = "<<nbins<<endl;
-	 ff <<"************************************"<<endl;
-	 ff <<"Integral"<<" "<<"counts"<<endl;
-
-	 ofstream ffP ("Juno_data_PED.txt");
-	 ffP <<"Juno PMT data - Pedestal"<<endl;
-	 ffP <<"Histogram of Integral vs. Counts - Pedestal"<<endl;
-	 ffP <<"No. of bins = "<<nbins<<endl;
-	 ffP <<"************************************"<<endl;
-	 ffP <<"Integral"<<" "<<"counts"<<endl;
-	 
-	 for (int i=0; i <Juno->GetNbinsX(); i++)
-		{
-	        ff << Juno->GetBinCenter(i) << "	" << Juno->GetBinContent(i) << endl; //write to file
-	  	}
-	  	ff.close();
-
-	 for (int i=0; i <JunoPED->GetNbinsX(); i++)
-		{
-	        ffP << JunoPED->GetBinCenter(i) << "	" << JunoPED->GetBinContent(i) << endl; //write to file
-	  	}
-	  	ffP.close();
+				 for (int i=0; i <JunoPED->GetNbinsX(); i++)
+					{
+				        ffP << JunoPED->GetBinCenter(i) << "	" << JunoPED->GetBinContent(i) << endl; //write to file
+				  	}
+				  	ffP.close();
 
 
-	TApp.Run();
-	return 0;
+				TApp.Run();
+				return 0;
+			}		
+
+	}			
+
+
 }
