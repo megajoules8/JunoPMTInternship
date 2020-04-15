1f5f3da301c8351849efa78ceeb0aa7d927fd81f
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-04-15 18:38:04 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -72,8 +72,8 @@ int main(int argc, char ** argv)
 				float bin_width = 0;
 
 				//definition of the histogram
-				TH1F *Juno = new TH1F("Juno", histname_LED, nbins , -8000, 1000);
-				TH1F *JunoPED = new TH1F("JunoPED", histname_PED, nbins , -8000, 1000);
+				TH1F *Juno = new TH1F("Juno", histname_LED, nbins , -10000, 4000);
+				TH1F *JunoPED = new TH1F("JunoPED", histname_PED, nbins , -10000, 4000);
 
 				for(int i=0; i < pmt_tree->GetEntries(); ++i)
 					{
