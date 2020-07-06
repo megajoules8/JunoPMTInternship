d4b48687c47ecdf465d8d21a085f2f2656676b48
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-07-07 00:03:23 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -88,7 +88,7 @@ int main(int argc, char ** argv)
 						t_min = 300;
 						t_max = 350;
 						h_max = 200;
-						h_min = -4000;
+						h_min = -2000;
 					} 
 				float t_min_PED = 0;
 				float t_max_PED = t_max-t_min;
