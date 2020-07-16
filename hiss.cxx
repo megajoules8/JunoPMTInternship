dab782b1e6075e34484dd1754e997f3febe32222
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-07-16 11:44:12 +0200

    Update hiss.cxx

--- hiss.cxx
+++ hiss.cxx
@@ -103,7 +103,7 @@ TH1F* hiss(TString Full_path, TString HV_Value, int index )
      }
     if (index == 0)
      {
-       Bin_Size = 8; 
+       Bin_Size = 16; 
        
      }
   hist-> Rebin(Bin_Size); 
