6f7f4fb889d7167846f7bb671097754bb579a036
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-07-20 10:36:01 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -100,6 +100,13 @@ int main(int argc, char ** argv)
 						h_max = 1000;
 						h_min = -6000;
 					}
+			else if (dat == 4230)
+					{
+						t_min = 730;
+						t_max = 800;
+						h_max = 1000;
+						h_min = -6000;
+					}
 				else if (dat == 248)
 					{
 						t_min = 300;
