9a8645732154aba543db467f23d9e65d0db251c7
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-07-24 14:58:12 +0200

    Update runfile_working_first.C

--- runfile_working_first.C
+++ runfile_working_first.C
@@ -253,7 +253,7 @@ if (index == 1)
 	 	Float_t POSITION_7[n];
 	 	Float_t POSITION_7_ERR[n];*/
 	 
-	 	Float_t PMT_DATA[][];
+	 	Float_t PMT_DATA[14][];
 	 
 	 	TMultiGraph  *mg  = new TMultiGraph();
 	 	for (int p = 1; p<8; ++p)
