dc48c0a43b2f3cb6e11f2a702ba00e8179450edc
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-10 20:42:51 +0200

    Update runfile_working_first.C

--- runfile_working_first.C
+++ runfile_working_first.C
@@ -476,7 +476,7 @@ if (index == 1)
 						}
 					
 						double cov;
-	 					cov = fit.mFFT->CovMatrix(fit.vals[6], fit.vals[6]);
+	 					cov = fit.mFFT->CovMatrix(0,0);
 		 				cout<<cov<<endl;
 			
 						
