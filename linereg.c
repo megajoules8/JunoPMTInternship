43e4f8c2d7c4159a2328f0b830b449e7868051eb
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-11 12:03:40 +0200

    Update linereg.c

--- linereg.c
+++ linereg.c
@@ -20,15 +20,16 @@ g.SetMarkerColor(kBlue);
 g.SetLineColor(kBlue);
 auto mycanvas = new TCanvas();
 g.DrawClone("APE");
-TF1 f("Linear law","[0]+x*[1]",.5,10.5);
-f.SetLineColor(kRed); f.SetLineStyle(2);
-g.Fit(&f);
+//TF1 f("Linear law","[0]+x*[1]",.5,10.5);
+TFitResultPtr r = g->Fit(“pol1”, “S”);
+r.SetLineColor(kRed); frSetLineStyle(2);
+//g.Fit(&f);
 f.DrawClone("Same");
 
-TMatrixD cov = f.GetCorrelationMatrix();
+/*TMatrixD cov = f.GetCorrelationMatrix();
 TMatrixD cor = f.GetCovarianceMatrix();
 cov.Print();
-cor.Print();
+cor.Print();*/
 return 0;
 }
 
