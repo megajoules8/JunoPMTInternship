7704458243772ee2d960cf24c0406d0d4dd243db
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-11 10:40:27 +0200

    Update runfile_working_first.C

--- runfile_working_first.C
+++ runfile_working_first.C
@@ -479,7 +479,7 @@ if (index == 1)
 						double cov;
 	 					cov = fit.mFFT->CovMatrix(3,3);
 		 				cout<<cov<<endl;
-						const TMatrixD ms = fit.mFFT->GetCovariantMatrix();
+						const TMatrixD ms = fit.mFFT->GetCovarianceMatrix();
  						ms.Print();
 						
 					}
