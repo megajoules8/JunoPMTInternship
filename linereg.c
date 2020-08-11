4f323e193e0e854609e079fbf8e61cd594d00d01
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-11 11:47:27 +0200

    Update linereg.c

--- linereg.c
+++ linereg.c
@@ -1,14 +1,30 @@
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TGraphErrors.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TArrow.h"
+#include "TLatex.h"
 
 void linereg()
 {
 Double_t x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 Double_t y[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-TGraph g = new TGraph((sizeof(x) / sizeof(Double_t)), x, y);
-g->Draw("A");
-TFitResultPtr r = g->Fit(“pol1”, “S”);
-TMatrixD cov = r->GetCorrelationMatrix();
-TMatrixD cor = r->GetCovarianceMatrix();
+
+TGraph g(10,x,y);
+graph.SetTitle("Measurement XYZ;lenght [cm];Arb.Units");
+auto mycanvas = new TCanvas();
+g.DrawClone("APE");
+TF1 f("Linear law","[0]+x*[1]",.5,10.5);
+g.Fit(&f);
+f.DrawClone("Same");
+TMatrixD cov = f->GetCorrelationMatrix();
+TMatrixD cor = f->GetCovarianceMatrix();
 cov.Print();
 cor.Print();
 return 0;
+}
+
+int main(){
+macro1();
 }
