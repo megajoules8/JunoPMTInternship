05bb683deac656084b2515ffc44c32c60173e189
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-11 11:50:04 +0200

    Update linereg.c

--- linereg.c
+++ linereg.c
@@ -5,6 +5,7 @@
 #include "TLegend.h"
 #include "TArrow.h"
 #include "TLatex.h"
+#include "TMatrixD.h"
 
 void linereg()
 {
@@ -12,7 +13,7 @@ Double_t x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 Double_t y[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
 
 TGraph g(10,x,y);
-graph.SetTitle("Measurement XYZ;lenght [cm];Arb.Units");
+g.SetTitle("Measurement XYZ;lenght [cm];Arb.Units");
 auto mycanvas = new TCanvas();
 g.DrawClone("APE");
 TF1 f("Linear law","[0]+x*[1]",.5,10.5);
