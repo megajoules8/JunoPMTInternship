b1d410ddbfba795b57ee96f52ea70e44880bd08c
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-11 11:16:59 +0200

    Update linereg.c

--- linereg.c
+++ linereg.c
@@ -4,8 +4,8 @@
 
 int QA2()
 {
-Double_t x[] = {1, 2, 3, 4, 5};
-Double_t y[] = {1.1, 1.9, 3.2, 3.9, 5.5};
+Double_t x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+Double_t y[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
 TGraph g = new TGraph((sizeof(x) / sizeof(Double_t)), x, y);
 g->Draw("A");
 TFitResultPtr r = g->Fit(“pol1”, “S”);
