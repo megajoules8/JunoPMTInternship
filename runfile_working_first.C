c9e1abffc54553cec223191f7eefa41e580be6cd
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-17 20:31:27 +0200

    Update runfile_working_first.C

--- runfile_working_first.C
+++ runfile_working_first.C
@@ -517,7 +517,7 @@ if (index == 1)
 						if ((fit.chi2r <= 3) && (fit.fit_status == 0) && (chi_red <= 3))	{ANGLES[count] = 15*a;  PMT_DATA[2*p-2][count] = Gfit;	PMT_DATA[2*p-1][count] = gainerror;   ++count;	STATUS = "Yes";}
 						else {STATUS = "No"; ++REM;}
 						gaindata << a*15 <<"  "<<fit.vals[3]<<"  "<<fit.errs[3]<<"  "<<fit.vals[7]<<"  "<<fit.errs[7]<<"  "<<fit.vals[6]<<"  "<<fit.errs[6]<<"  "<<fit.vals[4]<<"  "<<fit.errs[4]<<"  "<<fit.vals[5]<<"	"<<fit.errs[5]<<"  "<< sig_reduced<<"  "<<sig_reduced_err<<"  "\
-						<<Gfit <<"  "<< gainerror<<"  "<< fit.chi2r<<"  "<<chi_red)<<" "<<"  "<<fit.fit_status<<"  "<<STATUS<<endl;
+						<<Gfit <<"  "<< gainerror<<"  "<< fit.chi2r<<"  "<<chi_red<<" "<<"  "<<fit.fit_status<<"  "<<STATUS<<endl;
 						cout << " Gain (DUQ) : " << Gfit <<" +/- "<< gainerror << endl;
 						BW = histo_LED->GetBinWidth(2);
 						cout << " Bin Width : " << BW << endl;
