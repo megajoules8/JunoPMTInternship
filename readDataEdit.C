9796efdc0c1b4dc46082d889e1dc7f3ffe9d94fe
Deshan332 <61500321+Deshan332@users.noreply.github.com>
2020-08-26 10:57:04 +0200

    Update readDataEdit.C

--- readDataEdit.C
+++ readDataEdit.C
@@ -75,7 +75,7 @@ int main(int argc, char ** argv)
 				float h_min;
 				if (dat == 3737)
 					{
-						nbins = 120;
+						nbins = 600;
 						t_min = 400;
 						t_max = 470;
 						h_max = 1000;
